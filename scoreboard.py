0961d5dff46ff1fea8acdac1137747c71af11cfd
Rebecca-Ingram <41595749+beckyxde@users.noreply.github.com>
2021-01-21 23:54:23 +0100

    Added high score calculator

--- scoreboard.py
+++ scoreboard.py
@@ -3,28 +3,39 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         """Creates a scoreboard - updates when snake collides with food"""
         super().__init__()
         self.score = 0
+        # opens and reads txt file
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
-    def increase_score(self):
-        self.score += 1
-        self.clear()
+    def reset(self):
+        """writes in txt file with updates high score - score will be saved in txt file"""
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
         self.update_scoreboard()
 
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
